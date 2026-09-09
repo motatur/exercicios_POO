58f6d28ffc51c32723bd08f2f8739a982d32eb14
arthureng23-netizen <arthur.eng23@gmail.com>
2026-09-08 22:24:58 -0300

    feat:add exercicios de herança

---
+++ heranca/src_/Desenvolvedor.java
@@ -0,0 +1,3 @@
+public class Desenvolvedor extends Funcionario {
+    
+}
---
+++ heranca/src_/Funcionario.java
@@ -0,0 +1,33 @@
+public abstract class Funcionario {
+    private String nome;
+    private String cpf;
+    private double salario;
+
+    public Funcionario(String nome, String cpf, double salario) {
+        
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("O nome não pode estar vazio.");
+        }
+        if (cpf == null || cpf.isBlank()) {
+            throw new IllegalArgumentException("O CPF não pode estar vazio.");
+        }
+        if (salario <= 0) {
+            throw new IllegalArgumentException("O salario não pode ser 0.");
+        }
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario; 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
